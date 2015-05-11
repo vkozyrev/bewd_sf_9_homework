3046a7143d0d43464e2655215b0d9b193c9ae165
Michael F <mikey@mikeys-MacBook-Pro-2.local>
2015-05-11 14:55:05 -0700

    fixed issues with plural beers for 1 bottle

--- MichaelFerguson-mikeyferg/ex_loop.rb
+++ MichaelFerguson-mikeyferg/ex_loop.rb
@@ -20,26 +20,33 @@ require 'pry-byebug'
 
 
 # using while loop
+
 # bottles_of_beer = 99
-#
 # while bottles_of_beer >= 0
-#   if bottles_of_beer > 0
-#     puts "#{bottles_of_beer} bottles of beer on the wall \n #{bottles_of_beer} bottles of beer! \n You take one down and pass it around,"
+#   if bottles_of_beer > 1
+#     puts "#{bottles_of_beer} bottles of beer on the wall \n #{bottles_of_beer} bottles of beer! \n You take one down and pass it around, #{bottles_of_beer -1} bottles of beer on the wall!"
 #
-#   elsif bottles_of_beer == 0
-#     puts "No more bottles of beer on the wall :-("
+#   elsif bottles_of_beer == 1
+#       puts "#{bottles_of_beer} bottle of beer on the wall \n #{bottles_of_beer} bottle of beer! \n You take one down and pass it around, No more bottles of beer on the wall :-("
 #   end
 #   bottles_of_beer -= 1
 # end
 
 
 #down_to method
-# 99.downto(1) { puts "#{bottles_of_beer} bottles of beer on the wall \n #{bottles_of_beer} bottles of beer! \n You take one down and pass it around," }
-#
-# puts "No more bottles of beer on the wall :-("
-
+# 99.downto(1) do |bottles_of_beer|
+#   if bottles_of_beer > 1
+#     puts "#{bottles_of_beer} bottles of beer on the wall \n #{bottles_of_beer} bottles of beer! \n You take one down and pass it around, #{bottles_of_beer -1} bottles of beer on the wall!"
+#   elsif bottles_of_beer == 1
+#     puts "#{bottles_of_beer} bottle of beer on the wall \n #{bottles_of_beer} bottle of beer! \n You take one down and pass it around, No more bottles of beer on the wall :-("
+#   end
+# end
 
 #reverse_each method
-(1..99).reverse_each { |bottles_of_beer| puts "#{bottles_of_beer} bottles of beer on the wall \n #{bottles_of_beer} bottles of beer! \n You take one down and pass it around,"}
-
-puts "No more bottles of beer on the wall :-("
+(1..99).reverse_each do |bottles_of_beer|
+  if bottles_of_beer > 1
+    puts "#{bottles_of_beer} bottles of beer on the wall \n #{bottles_of_beer} bottles of beer! \n You take one down and pass it around, #{bottles_of_beer -1} bottles of beer on the wall!"
+  elsif bottles_of_beer == 1
+    puts "#{bottles_of_beer} bottle of beer on the wall \n #{bottles_of_beer} bottle of beer! \n You take one down and pass it around, No more bottles of beer on the wall :-("
+  end
+end
