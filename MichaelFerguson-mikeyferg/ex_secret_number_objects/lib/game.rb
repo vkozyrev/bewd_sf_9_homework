561cf75de45cc62c4021f593b4591499f705d539
Michael F <mikey@mikeys-MacBook-Pro-2.local>
2015-05-19 15:43:46 -0700

    adding unit 6 homework

---
+++ MichaelFerguson-mikeyferg/ex_secret_number_objects/lib/game.rb
@@ -0,0 +1,44 @@
+require_relative 'person'
+require_relative 'secret_number'
+require 'pry'
+require 'pry-byebug'
+
+class Game
+# this class will be complexå
+# we need to write logic to initialize a new game, and run the game loop
+# we'll want to write a few separate methods for unique tasks so that our
+# code is structured properly
+  attr_accessor :secret_num, :user_num, :guesses, :player
+
+  def initialize(player)
+    @secret_num = SecretNumber.new().rand_num
+    @user_num = nil
+    @guesses = 0
+    @player = player
+  end
+
+  def pick_num
+    puts "Please guess a number!"
+    @user_num = gets.chomp.to_i
+    @guesses += 1
+  end
+
+  def start
+    if @guesses < 1
+      player.print_name
+    end
+    pick_num
+    puts "secret_num is #{secret_num}"
+    puts "user_num is #{user_num}"
+    puts "guesses is #{guesses}"
+    if @secret_num == @user_num
+       puts "Good guess #{player.name}.  You are correct.  The secret_number is #{secret_num}.  It took you #{guesses} guesses."
+    elsif @secret_num < @user_num
+      puts "Too high.  Guess again"
+      start
+    else
+      puts "Too low.  Guess again."
+      start
+     end
+  end
+end
