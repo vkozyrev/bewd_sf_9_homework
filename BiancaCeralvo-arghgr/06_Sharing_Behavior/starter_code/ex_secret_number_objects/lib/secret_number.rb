58a858d2865736c262b0827ea13572efd8a85cdc
Bianca <tenmillionpeople@gmail.com>
2015-05-18 21:44:21 -0700

    Added Lesson 6 exercises and homework

---
+++ BiancaCeralvo-arghgr/06_Sharing_Behavior/starter_code/ex_secret_number_objects/lib/secret_number.rb
@@ -0,0 +1,14 @@
+class SecretNumber
+# this class will generate our secret number for us
+  attr_accessor :number_array, :random_number
+
+  def initialize
+    @number_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    @random_number = @number_array.sample
+    return @random_number
+  end
+
+  def to_s
+    puts "The secret number is #{@random_number}"
+  end
+end
