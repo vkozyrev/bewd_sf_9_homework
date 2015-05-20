2bd9417b7eab6e1453f3b2637e518172af2e52ae
Michael F <mikey@mikeys-MacBook-Pro-2.local>
2015-05-20 11:16:15 -0700

    changed guess count variable to a class variable and method

--- MichaelFerguson-mikeyferg/ex_secret_number_objects/lib/game.rb
+++ MichaelFerguson-mikeyferg/ex_secret_number_objects/lib/game.rb
@@ -8,29 +8,36 @@ class Game
 # we need to write logic to initialize a new game, and run the game loop
 # we'll want to write a few separate methods for unique tasks so that our
 # code is structured properly
-  attr_accessor :secret_num, :user_num, :guesses, :player
+  attr_accessor :secret_num, :user_num, :player, #:guesses
 
   def initialize(player)
     @secret_num = sec_num_generator
     @user_num = nil
-    @guesses = 0
+    #@guesses = 0
     @player = player
+  end
+  @@total_guesses = 0
+
+  def self.total_guesses_ever
+    @@total_guesses += 1
   end
 
   def pick_num
     puts "Please guess a number!"
     @user_num = gets.chomp.to_i
-    @guesses += 1
+    #@guesses += 1
+    Game.total_guesses_ever
   end
 
   def start
-    player.print_name if @guesses < 1
+    player.print_name if @@total_guesses < 1
     pick_num
     puts "secret_num is #{secret_num}"
     puts "user_num is #{user_num}"
-    puts "guesses is #{guesses}"
+    puts "guesses is #{@@total_guesses}"
     if @secret_num == @user_num
-       puts "Good guess #{player.name}.  You are correct.  The secret_number is #{secret_num}.  It took you #{guesses} guesses."
+       puts "Good guess #{player.name}.  You are correct.  The secret_number is #{secret_num}.  It took you #{@@total_guesses} guesses."
+       #puts "Total guesses ever in game- #{@@total_guesses}"
     elsif @secret_num < @user_num
       puts "Too high.  Guess again"
       start
