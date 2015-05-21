93b2aa510ccc748614d1561579788e2c1f5edf3f
bgamwell <bgamwell@gmail.com>
2015-05-20 17:40:05 -0700

    Secret number homework 05/20

--- BrennanGamwell-bgamwell/06_Sharing_Behavior/starter_code/ex_secret_number_objects/lib/person.rb
+++ BrennanGamwell-bgamwell/06_Sharing_Behavior/starter_code/ex_secret_number_objects/lib/person.rb
@@ -1,3 +1,14 @@
+# Player
+#  Should initialize the player's name.
+#  Use gets.strip to get the player's name and assign it to the instance variable name
+
+require 'pry'
+require 'pry-byebug'
+
 class Person
-# this class will likely be simple, and just remember its name
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 end
