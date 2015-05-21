26746483e091eda7e75382abc83334a75a977850
Vishal Amin <VishalRAmin@gmail.com>
2015-05-20 18:31:17 -0700

    send

--- VishalAmin-VishalRAmin/ex_secret_number_objects/lib/secret_number.rb
+++ VishalAmin-VishalRAmin/ex_secret_number_objects/lib/secret_number.rb
@@ -5,15 +5,16 @@ class SecretNumber # this class will generate our secret number for us
 
   attr_accessor :number
 
-  def initialize (number)
-    @number = rand(1..10).to_i
+  def initialize(number)
+    @number = all_nums
   end
 
-  # def to_s
-  #   "Your secret number is #{number}."
-  # end
+  def randomizer
+    number.sample
+  end
+
 end
 
-secretnumber = secretnumber.new
+secretnumber = SecretNumber.new
 
-# all_nums = [1,2,3,4,5,6,7,8,9,10]
+all_nums = [1,2,3,4,5,6,7,8,9,10]
