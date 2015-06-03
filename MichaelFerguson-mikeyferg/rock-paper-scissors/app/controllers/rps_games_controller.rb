560424a9e4b27ec07bb05ff2ac864276b04520a5
Michael F <mikey@mikeys-MacBook-Pro-2.local>
2015-06-02 20:14:27 -0700

    added gifs to ppartials and updated logic to reflect actual RPS rules

--- MichaelFerguson-mikeyferg/rock-paper-scissors/app/controllers/rps_games_controller.rb
+++ MichaelFerguson-mikeyferg/rock-paper-scissors/app/controllers/rps_games_controller.rb
@@ -16,11 +16,43 @@ class RpsGamesController < ApplicationController
     [{number: 0, image_link: "rock_paper_scissors/rock.jpg"}, {number: 1, image_link: "rock_paper_scissors/paper.jpg"}, {number: 2, image_link: "rock_paper_scissors/scissors.jpg"}]
   end
 
+  # def is_winner?
+  #   if @computer_guess == @player_pick
+  #     "win"
+  #   else
+  #     "lose"
+  #   end
+  # end
+
   def is_winner?
-    if @computer_guess == @player_pick
-      "win"
-    else
-      "lose"
+    if @player_pick[:number] == 0
+      if @computer_guess[:number] == 0
+        "tie"
+      elsif @computer_guess[:number] == 1
+        "lose"
+      elsif @computer_guess[:number] == 2
+        'win'
+      end
+    elsif @player_pick[:number] == 1
+      if @computer_guess[:number] == 0
+        "win"
+      elsif @computer_guess[:number] == 1
+        "tie"
+      elsif @computer_guess[:number] == 2
+        "lose"
+      end
+    elsif @player_pick[:number] == 2
+      if @computer_guess[:number] == 0
+        "lose"
+      elsif @computer_guess[:number] == 1
+        "win"
+      elsif @computer_guess[:number] == 2
+        "tie"
+      end
     end
   end
+
+
+
+
 end
