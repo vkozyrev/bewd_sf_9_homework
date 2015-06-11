02ada0bca1bd623be88d9b0fadcee66a7216d807
Michael F <mikey@mikeys-MacBook-Pro-2.local>
2015-06-11 14:59:09 -0700

    unit 12 movie_app part3 w/some styling

--- MichaelFerguson-mikeyferg/movie_app/config/routes.rb
+++ MichaelFerguson-mikeyferg/movie_app/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
+
+  get 'home', to: "static_pages#home"
+  get 'test', to: "static_pages#test"
+  get 'about', to: "static_pages#about"
+
   resources :movies
   resources :reviews
-  root 'movies#index'
+  root 'static_pages#home'
 end
